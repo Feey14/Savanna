bffcb5d8f20edc9bd1cc462076c0fcb797e8a5cc
Edgars Bondars <e.bobondars@gmail.com>
2020-07-09 11:18:42 +0300

    Added AnimalsClassLibrary

---
+++ Gazelle.cs
@@ -0,0 +1,11 @@
+﻿using AnimalTypeClassLibrary;
+
+namespace AnimalsClassLibrary
+{
+    public class Gazelle : NonPredator
+    {
+        public override char AnimalSymbol => 'G';
+        public override double Health { get; set; } = 20;
+        public override int VisionRange => 6;
+    }
+}
