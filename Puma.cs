d1c8ac99110cecc7be0d4b7fbcaf7b72a48ba5d0
Edgar B <feey14@mail.ru>
2020-07-13 15:20:45 +0300

    first commit

--- Puma.cs
+++ Puma.cs
@@ -4,8 +4,19 @@ namespace AnimalsClassLibrary
 {
     internal class Puma : Predator
     {
+        /// <summary>
+        /// Defining AnimalSymbol that represents this animal in game visualisation
+        /// </summary>
         public override char AnimalSymbol => 'P';
+
+        /// <summary>
+        /// overriding Animal's Health with which he starts game with
+        /// </summary>
         public override double Health { get; set; } = 20;
+
+        /// <summary>
+        /// overriding Animal's Visionrange, animals sees his victim/attacker within this range
+        /// </summary>
         public override int VisionRange => 6;
     }
 }
--- Tiger.cs
+++ Tiger.cs
@@ -4,8 +4,19 @@ namespace AnimalsClassLibrary
 {
     internal class Tiger : Predator
     {
+        /// <summary>
+        /// Defining AnimalSymbol that represents this animal in game visualisation
+        /// </summary>
         public override char AnimalSymbol => 'T';
+
+        /// <summary>
+        /// overriding Animal's Health with which he starts game with
+        /// </summary>
         public override double Health { get; set; } = 20;
+
+        /// <summary>
+        /// overriding Animal's Visionrange, animals sees his victim/attacker within this range
+        /// </summary>
         public override int VisionRange => 6;
     }
 }
